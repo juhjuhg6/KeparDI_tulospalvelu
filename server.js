98e930cc9306c253a686176de777bef4056a4c18
Juha Lilja <juha.v.lilja@gmail.com>
2020-10-09 18:04:03 +0300

    luotu yhteys tietokantaan

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// yhteys tietokantaan
+const db = process.env.MONGODB_URI
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(() => console.log('Yhdistetty tietokantaan.'))
+  .catch(err => console.log(`Virhe tietokantaan yhdistämisessä: ${err}`))
 
 app.get('/', (req, res) => {
   res.send('Hello, World!')
