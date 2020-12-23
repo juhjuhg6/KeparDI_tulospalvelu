47f85f36456eb44e49f98195c3d38da3340510ed
Juha Lilja <juha.v.lilja@gmail.com>
2020-12-23 23:08:14 +0200

    poistettu turhat toSting() kutsut

--- client/src/Lahtoajat.js
+++ client/src/Lahtoajat.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Lähtöaika from './Lahtoaika'
 
 function Lähtöajat({aktiivinenKausi, kilpailu, setKilpailu}) {
-  return(
+  return (
     <div>
       {kilpailu.sarjat
-      ? kilpailu.sarjat.map(sarja => <div key={sarja._id.toString()}>
+      ? kilpailu.sarjat.map(sarja => <div key={sarja._id}>
           <h4>{sarja.nimi}</h4>
           <table>
             <thead><tr><th>Nimi</th><th>Lähtöaika</th></tr></thead>
             <tbody>
-              {sarja.kilpailijat.map(kilpailija => <Lähtöaika  key={kilpailija._id.toString()}
+              {sarja.kilpailijat.map(kilpailija => <Lähtöaika  key={kilpailija._id}
                 aktiivinenKausi={aktiivinenKausi} kilpailija={kilpailija}
                 kilpailu={kilpailu} setKilpailu={setKilpailu} sarja={sarja} />)}
             </tbody>
