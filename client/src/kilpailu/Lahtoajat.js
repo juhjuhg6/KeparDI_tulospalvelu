9b6a3ad012264a6860a6367f1f83bf32d87dfce1
Juha Lilja <juha.v.lilja@gmail.com>
2021-04-16 21:39:27 +0300

    lisätty ohjeteksti, että uuteen kilpailuun tulee lisätä ensimmäisenä sarja

--- client/src/kilpailu/Lahtoajat.js
+++ client/src/kilpailu/Lahtoajat.js
@@ -29,46 +29,51 @@ function Lähtöajat() {
 
   return (
     <div>
-      {!kilpailijanLisäys && !lähtöaikojenMuokkaus
+      {kilpailu.sarjat.length
         ?
-          kirjauduttu
-            ?
-              <>
-                <button onClick={() => setKilpailijanLisäys(true)} className='btn-yellow'>Lisää kilpailija</button>
-                <button onClick={() => setLähtöaikojenMuokkaus(true)} className='btn-yellow'>Arvo lähtöajat</button>
-              </>
-            :
-              (kilpailu.ilmoittautuminenDl && moment(kilpailu.ilmoittautuminenDl) > Date.now()) &&
-                <button onClick={() => setKilpailijanLisäys(true)} className='btn-green'>Ilmoittaudu</button>
+          <>
+            {!kilpailijanLisäys && !lähtöaikojenMuokkaus
+              ?
+                kirjauduttu
+                  ?
+                    <>
+                      <button onClick={() => setKilpailijanLisäys(true)} className='btn-yellow'>Lisää kilpailija</button>
+                      <button onClick={() => setLähtöaikojenMuokkaus(true)} className='btn-yellow'>Arvo lähtöajat</button>
+                    </>
+                  :
+                    (kilpailu.ilmoittautuminenDl && moment(kilpailu.ilmoittautuminenDl) > Date.now()) &&
+                      <button onClick={() => setKilpailijanLisäys(true)} className='btn-green'>Ilmoittaudu</button>
+              :
+                kilpailijanLisäys
+                  ? <UusiKilpailija setKilpailijanLisäys={setKilpailijanLisäys} />
+                  : <LähtöaikojenAsetus setLähtöaikojenMuokkaus={setLähtöaikojenMuokkaus} />
+            }
+            <div className='flex-container'><div className='table-container'>
+              {kilpailu.sarjat.map(sarja => <div key={sarja._id}>
+                <h4>{sarja.nimi}</h4>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Nimi</th><th>Lähtöaika</th>
+                      {kirjauduttu && <th></th>}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {sarja.kilpailijat.sort((a, b) => {
+                      if (a.kilpailut[kilpailu._id].lahtoaika < b.kilpailut[kilpailu._id].lahtoaika) return -1
+                      if (a.kilpailut[kilpailu._id].lahtoaika > b.kilpailut[kilpailu._id].lahtoaika) return 1
+                      return 0
+                    }).map(kilpailija => <Lähtöaika  key={kilpailija._id}
+                      kilpailija={kilpailija} sarja={sarja} momentFormat={momentFormat} />)}
+                  </tbody>
+                </table>
+              </div>)}
+            </div></div>
+          </>
         :
-          kilpailijanLisäys
-            ? <UusiKilpailija setKilpailijanLisäys={setKilpailijanLisäys} />
-            : <LähtöaikojenAsetus setLähtöaikojenMuokkaus={setLähtöaikojenMuokkaus} />
+          <p>Lisää ainakin yksi sarja kilpailuun painamalla:<br/><i>Muokkaa kilpailua</i> {'>'} <i>Muokkaa sarjoja</i>.</p>
       }
-      <div className='flex-container'><div className='table-container'>
-        {kilpailu.sarjat &&
-          kilpailu.sarjat.map(sarja => <div key={sarja._id}>
-            <h4>{sarja.nimi}</h4>
-            <table>
-              <thead>
-                <tr>
-                  <th>Nimi</th><th>Lähtöaika</th>
-                  {kirjauduttu && <th></th>}
-                </tr>
-              </thead>
-              <tbody>
-                {sarja.kilpailijat.sort((a, b) => {
-                  if (a.kilpailut[kilpailu._id].lahtoaika < b.kilpailut[kilpailu._id].lahtoaika) return -1
-                  if (a.kilpailut[kilpailu._id].lahtoaika > b.kilpailut[kilpailu._id].lahtoaika) return 1
-                  return 0
-                }).map(kilpailija => <Lähtöaika  key={kilpailija._id}
-                  kilpailija={kilpailija} sarja={sarja} momentFormat={momentFormat} />)}
-              </tbody>
-            </table>
-          </div>)
-        }
-      </div>
-    </div></div>
+    </div>
   )
 }
 
